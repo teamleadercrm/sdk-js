1b6dc2ce20ce96fb83f49ac950fdd8e856b2a6c0
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-02-12 14:22:23 +0100

    Add initial project setup
    - Rollup (ES / CJS / UMD builds)
    - editorconfig
    - eslint / prettier
    - babel

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    output: {
+      name: 'api',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      resolve(),
+      commonjs(),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/main.js',
+    // external: ['ms'],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+];
