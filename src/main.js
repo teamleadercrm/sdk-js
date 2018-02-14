3caf33c7b3ca553e9adc5fe14b84f399b4a3461a
David Vanpachtenbeke <david@codifly.be>
2018-02-14 16:13:33 +0100

    feat: Adding CRM domains to API.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import contacts from './domains/contacts.js';
+import companies from './domains/companies.js';
+import businessTypes from './domains/businessTypes.js';
+import quotations from './domains/quotations.js';
+import tags from './domains/tags.js';
+import dealPhases from './domains/dealPhases.js';
 
 const createGetAccessToken = config => {
   const { accessToken, getAccessToken } = config;
@@ -14,8 +19,15 @@ const API = config => {
   const getAccessToken = createGetAccessToken(config);
   const { baseUrl = 'https://api.teamleader.eu' } = config;
 
+  const configuration = { getAccessToken, baseUrl };
+
   return {
-    contacts: contacts({ getAccessToken, baseUrl }),
+    contacts: contacts(configuration),
+    companies: companies(configuration),
+    businessTypes: businessTypes(configuration),
+    quotations: quotations(configuration),
+    tags: tags(configuration),
+    dealPhases: dealPhases(configuration),
   };
 };
 
