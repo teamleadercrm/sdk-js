b69fe25759386530230b38a17138caae34967de0
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-02-15 14:03:03 +0100

    Add response check
    - check if response is not empty before turning it into json

--- src/utils/call.js
+++ src/utils/call.js
@@ -1,2 +1,9 @@
-const call = (url, options) => fetch(url, options).then(d => d.json());
+const responseHandler = response => {
+  if (!response) {
+    return response;
+  }
+  return response.json();
+};
+
+const call = (url, options) => fetch(url, options).then(responseHandler);
 export default call;
