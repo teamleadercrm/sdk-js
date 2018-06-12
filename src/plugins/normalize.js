0ff2d7df8212330cf21e20a9b06e31c022e8b31f
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-06-12 10:07:18 +0200

    Add array normalization

--- src/plugins/normalize.js
+++ src/plugins/normalize.js
@@ -1,1 +1,4 @@
-export default ({ data }) => data.reduce((o, d) => ({ ...o, byId: { ...o.byId, [d.id]: d } }), {});
+export default ({ data }) => {
+  const dataArray = Array.isArray(data) ? data : [data];
+  return dataArray.reduce((o, d) => ({ ...o, byId: { ...o.byId, [d.id]: d } }), {});
+};
