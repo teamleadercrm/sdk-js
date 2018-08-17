31d29571eb81c0c63f21b893a905fa58500f379b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-08-17 10:28:28 +0200

    Add test for response plugins

--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,23 @@
 import API from '../src/main';
+import camelCase from '../src/plugins/camelCase';
+import normalize from '../src/plugins/normalize';
 
 describe('fetch response handling', () => {
+  const mockFetch = desiredResponse => (window.fetch = () => Promise.resolve(desiredResponse));
+  const response = ({ ok, statusText, status, contentType, json, text }) => {
+    const headers = new Map();
+    headers.set('content-type', contentType);
+
+    return {
+      ok,
+      headers,
+      status,
+      statusText,
+      json: () => Promise.resolve(json),
+      text: () => Promise.resolve(text),
+    };
+  };
+
   it('shoud add the customActions to the correct domains', () => {
     const api = API({
       getAccessToken: () => 'thisisatoken', // async or sync function
@@ -29,6 +46,27 @@ describe('fetch response handling', () => {
 
     const dealPhasesMethods = ['list'];
     expect(Object.keys(api.dealPhases).sort()).toEqual(dealPhasesMethods.sort());
+  });
+
+  it('should run the correct response plugins', () => {
+    mockFetch(
+      response({
+        ok: true,
+        contentType: 'application/json',
+        json: { data: { id: '84845512', name: 'john', last_name: 'doe' } },
+      }),
+    );
+
+    const api = API({
+      getAccessToken: () => 'thisisatoken', // async or sync function
+      plugins: {
+        response: [camelCase],
+      },
+    });
+
+    api.contacts.info({ userId: '84989' }, { response: [normalize] }).then(data => {
+      expect(data).toEqual({ byId: { '84845512': { id: '84845512', lastName: 'doe', name: 'john' } } });
+    });
   });
 
   it('shoud include all domains', () => {
