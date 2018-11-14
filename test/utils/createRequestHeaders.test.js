70ed358027ce9173e09206977019f0af71408b7b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-11-14 14:46:36 +0100

    Create createRequestHeaders function

--- test/utils/createRequestHeaders.test.js
+++ test/utils/createRequestHeaders.test.js
@@ -1,33 +1,15 @@
-import createHeaders from '../../src/utils/createHeaders';
+import createRequestHeaders from '../../src/utils/createRequestHeaders';
 
 describe(`create header object`, () => {
-  it(`should handle passing in a contentType`, async () => {
-    const config = {
-      getAccessToken: () => 'token',
-      contentType: 'application/xml',
-    };
-
-    const headers = {
-      'Content-Type': 'application/xml',
-      Authorization: `Bearer token`,
-    };
-
-    await expect(createHeaders(config)).resolves.toEqual(headers);
-  });
-
   it(`should be able to pass in a sync getAccessToken function`, async () => {
     const getAccessToken = () => 'token';
-
-    const config = {
-      getAccessToken,
-    };
 
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer token`,
     };
 
-    await expect(createHeaders(config)).resolves.toEqual(headers);
+    await expect(createRequestHeaders(getAccessToken)).resolves.toEqual(headers);
   });
 
   it(`should be able to pass in a async getAccessToken function`, async () => {
@@ -38,15 +20,11 @@ describe(`create header object`, () => {
       return 'token';
     };
 
-    const config = {
-      getAccessToken,
-    };
-
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer token`,
     };
 
-    await expect(createHeaders(config)).resolves.toEqual(headers);
+    await expect(createRequestHeaders(getAccessToken)).resolves.toEqual(headers);
   });
 });
