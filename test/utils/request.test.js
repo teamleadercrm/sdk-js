e4cc83e18bebee9f9df3572dea882bbd28d4d5f3
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-11-14 14:40:29 +0100

    Fix request tests

--- test/utils/request.test.js
+++ test/utils/request.test.js
@@ -1,4 +1,4 @@
-import call from '../../src/utils/call';
+import request from '../../src/utils/request';
 import { camelCase } from '../../src/main';
 
 const mockFetch = desiredResponse => (window.fetch = () => Promise.resolve(desiredResponse));
@@ -12,7 +12,6 @@ const response = ({ ok, statusText, status, contentType, json, text }) => {
     status,
     statusText,
     json: () => Promise.resolve(json),
-    text: () => Promise.resolve(text),
   };
 };
 
@@ -26,24 +25,10 @@ describe('fetch response handling', () => {
       }),
     );
 
-    call().then(jsonResponse => {
+    request().then(jsonResponse => {
       expect(jsonResponse).toEqual({
         foo: 'bar',
       });
-    });
-  });
-
-  it('returns text when the response was successful', () => {
-    mockFetch(
-      response({
-        ok: true,
-        contentType: 'text/plain',
-        text: 'foo bar',
-      }),
-    );
-
-    call().then(textResponse => {
-      expect(textResponse).toEqual('foo bar');
     });
   });
 
@@ -56,7 +41,7 @@ describe('fetch response handling', () => {
       }),
     );
 
-    call(undefined, undefined, { plugins: { response: [camelCase] } }).then(jsonResponse => {
+    request(undefined, undefined, { plugins: { response: [camelCase] } }).then(jsonResponse => {
       expect(jsonResponse).toEqual({ data: { userId: 'bar' } });
     });
   });
@@ -74,32 +59,13 @@ describe('fetch response handling', () => {
       }),
     );
 
-    call().catch(e => {
+    request().catch(e => {
       expect(e.status).toEqual(500);
       expect(e.statusText).toEqual('There was an error 500');
       expect(e.message).toEqual('There was an error 500');
       expect(e.body).toEqual({
         errors: ['There was an error'],
       });
-    });
-  });
-
-  it('throws an error with text if the response was unsuccessful', () => {
-    mockFetch(
-      response({
-        ok: false,
-        contentType: 'text/plain',
-        text: 'There was an error',
-        status: 500,
-        statusText: 'There was an error 500',
-      }),
-    );
-
-    call().catch(e => {
-      expect(e.status).toEqual(500);
-      expect(e.message).toEqual('There was an error 500');
-      expect(e.statusText).toEqual('There was an error 500');
-      expect(e.body).toEqual('There was an error');
     });
   });
 });
