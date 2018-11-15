085d3475b39d7060ab2b0cbb0c0ca7f3ff55c9fc
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-11-15 09:27:16 +0100

    Fix test

--- src/utils/createFetchOptions.js
+++ src/utils/createFetchOptions.js
@@ -1,11 +1,11 @@
 import createRequestHeaders from './createRequestHeaders';
 import flow from './flow';
 
-export default async ({ configuration, parameters } = {}) => {
+export default async ({ configuration, parameters = {} } = {}) => {
   const { getAccessToken, plugins: { request: requestPlugins = [] } = {}, fetchAll } = configuration;
 
   if (fetchAll === true) {
-    parameters = { ...parameters, page: { size: 100 } };
+    parameters = { ...parameters, page: { ...(parameters.page || {}), size: 100 } };
   }
 
   return {
