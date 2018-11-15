a4e7a10b3d106da70dc68aaf0b714366f7f30547
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-11-15 13:36:18 +0100

    Pass correct initial pagination info

--- src/request/sequentialRequests.js
+++ src/request/sequentialRequests.js
@@ -2,6 +2,15 @@ import createFetchOptions from './createFetchOptions';
 import checkStatus from './checkStatus';
 
 export default async (requestUrl, parameters, configuration) => {
-  const fetchOptions = await createFetchOptions({ configuration, parameters });
+  const pageNumber = 1;
+
+  const fetchOptions = await createFetchOptions({
+    configuration,
+    parameters: {
+      ...parameters,
+      page: { size: 100, number: pageNumber },
+    },
+  });
+
   return fetch(requestUrl, fetchOptions).then(checkStatus);
 };
