32e1b051e6fd9a63b3870882f50eaee9d18e42cf
Kemosabert <bert.coppens14@gmail.com>
2018-12-03 12:45:19 +0100

    :sparkles: Add a function to check whether or not the provided api version is in the future

--- src/utils/validateApiVersion.js
+++ src/utils/validateApiVersion.js
@@ -1,4 +1,4 @@
-export default version => {
+export const validateApiVersion = version => {
   const API_VERSION_FORMAT_REGEX = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/g;
 
   if (!API_VERSION_FORMAT_REGEX.test(version)) {
@@ -8,4 +8,12 @@ export default version => {
   }
 
   return true;
+};
+
+export const isVersionDateInTheFuture = version => {
+  const today = new Date();
+  const versionDateIndexes = version.split('-').map(date => parseInt(date, 10));
+  const apiVersionDate = new Date(versionDateIndexes[0], versionDateIndexes[1] - 1, versionDateIndexes[2]);
+
+  return today - apiVersionDate < 0;
 };
--- test/utils/validateApiVersion.test.js
+++ test/utils/validateApiVersion.test.js
@@ -1,4 +1,4 @@
-import validateApiVersion from '../../src/utils/validateApiVersion';
+import { validateApiVersion, isVersionDateInTheFuture } from '../../src/utils/validateApiVersion';
 
 describe('Validate the provided API version', () => {
   it('should allow the provided API version', () => {
@@ -9,5 +9,11 @@ describe('Validate the provided API version', () => {
     expect(() => {
       validateApiVersion('01-05-20188');
     }).toThrow();
+  it('should recognize that the version date is in the past', () => {
+    expect(isVersionDateInTheFuture('2018-09-01')).toBe(false);
+  });
+
+  it('should recognize that the version date is in the future', () => {
+    expect(isVersionDateInTheFuture('2030-12-31')).toBe(true);
   });
 });
