e0785d1130fbc11570cc450649ec96a5597d2ec7
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2019-01-24 16:03:38 +0100

    Remove return

--- src/utils/request/checkStatus.js
+++ src/utils/request/checkStatus.js
@@ -1,14 +1,13 @@
 import getResponseData from './getResponseData';
 import FetchError from './FetchError';
 
-const checkStatus = response => {
-  return getResponseData(response).then(data => {
+const checkStatus = response =>
+  getResponseData(response).then(data => {
     if (!response.ok) {
       throw new FetchError(response.status, response.statusText, data);
     }
 
     return data;
   });
-};
 
 export default checkStatus;
