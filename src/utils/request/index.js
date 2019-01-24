a066ff0e22f45178071e1fca30b1fb1a15e472e9
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2019-01-24 15:53:07 +0100

    export request from index

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -1,12 +1,2 @@
-import checkStatus from './checkStatus';
-import applyPlugins from '../applyPlugins';
-
-const request = (url, fetchOptions = {}, configuration = {}) => {
-  const { plugins: { response: responsePlugins = [] } = {} } = configuration;
-
-  return fetch(url, fetchOptions)
-    .then(checkStatus)
-    .then(data => applyPlugins(data, responsePlugins));
-};
-
+import request from './request';
 export default request;
