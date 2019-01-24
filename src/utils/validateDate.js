1edb11a2ba498fae7a4baf295591fdf5d6a74417
Kemosabert <bert.coppens14@gmail.com>
2019-01-24 11:52:30 +0100

    :recycle: Check if the provided day is possible in the provided month

--- src/utils/validateDate.js
+++ src/utils/validateDate.js
@@ -1,9 +1,25 @@
 export const isValidDate = date => {
   const API_VERSION_FORMAT_REGEX = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/g;
 
-  if (!API_VERSION_FORMAT_REGEX.test(date)) {
-    return false;
-  }
+  return API_VERSION_FORMAT_REGEX.test(date) && isValid(date);
+};
 
-  return true;
+const daysInMonth = (month, year) => {
+  switch (month) {
+    case 2:
+      return (year % 4 === 0 && year % 100) || year % 400 === 0 ? 29 : 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 31;
+  }
+};
+
+const isValid = date => {
+  const [year, month, day] = date.split('-').map(value => parseInt(value, 10));
+
+  return month >= 1 && month <= 12 && day > 0 && day <= daysInMonth(month, year);
 };
--- test/utils/validateDate.test.js
+++ test/utils/validateDate.test.js
@@ -8,4 +8,12 @@ describe('Test the date validator', () => {
   it('should validate the format of the provided date as false', () => {
     expect(isValidDate('01-01-2019')).toBe(false);
   });
+
+  it('should validate the provided date as true', () => {
+    expect(isValidDate('2020-02-29')).toBe(true);
+  });
+
+  it('should validate the provided date as false', () => {
+    expect(isValidDate('2019-02-29')).toBe(false);
+  });
 });
