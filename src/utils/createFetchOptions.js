96df94d92bd92ab27fe0e238f66a1a887ece014f
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2019-01-24 15:51:35 +0100

    Use applyPlugins in createFetchOptions

--- src/utils/createFetchOptions.js
+++ src/utils/createFetchOptions.js
@@ -1,17 +1,12 @@
 import createRequestHeaders from './createRequestHeaders';
-import flow from './flow';
+import applyPlugins from './applyPlugins';
 
 export default async ({ configuration, parameters } = {}) => {
   const { getAccessToken, plugins: { request: requestPlugins = [] } = {}, version } = configuration;
 
   return {
     headers: await createRequestHeaders({ getAccessToken, version }),
-    body: JSON.stringify(
-      flow(
-        parameters,
-        requestPlugins,
-      ),
-    ),
+    body: JSON.stringify(applyPlugins(parameters, requestPlugins)),
     method: 'POST',
   };
 };
