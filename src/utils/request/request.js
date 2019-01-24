9ba3b4656dd02bb3b77e4af2094f44cf0e60bf2e
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2019-01-24 17:13:23 +0100

    Create initial code skeleton

--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -2,7 +2,16 @@ import singleRequest from './singleRequest';
 import applyPlugins from '../applyPlugins';
 
 const request = (url, fetchOptions = {}, configuration = {}) => {
-  const { plugins: { response: responsePlugins = [] } = {} } = configuration;
+  const { plugins: { response: responsePlugins = [] } = {}, fetchAll = false } = configuration;
+
+  if (fetchAll) {
+    // do a first initial request
+    // request the other pages in parallel
+    // merge the data
+    // apply the plugins
+    return Promise.resolve('todo');
+  }
+
   return singleRequest(url, fetchOptions).then(data => applyPlugins(data, responsePlugins));
 };
 
