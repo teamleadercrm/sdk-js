c43b0bd2d9ced59327a69c285a86174cbd51eaa4
Rathes Sachchithananthan <sachchi@rathes.de>
2019-05-08 11:20:35 +0200

    👌🏾 Simplify map

--- src/utils/request/fetchAllRequest.js
+++ src/utils/request/fetchAllRequest.js
@@ -3,8 +3,7 @@ import singleRequest from './singleRequest';
 const fetchAllRequest = async (url, amountOfRequests, fetchOptions = {}) => {
   const data = await Promise.all(
     [...new Array(amountOfRequests - 1)] // -1 because we already did a firstRequest
-      .map((_, i) => i + 2) // 2, 3, 4, ...
-      .map((_, number) => singleRequest(url, { ...fetchOptions, page: { number } })),
+      .map((_, i) => singleRequest(url, { ...fetchOptions, page: { number: i + 2 } })),
   );
   return data;
 };
