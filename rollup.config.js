90a0ec8a1482f1562ae918ae2379580a008922cf
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2019-05-09 11:28:04 +0200

    Remove bundling for commonjs / es

--- rollup.config.js
+++ rollup.config.js
@@ -16,12 +16,12 @@ export default [
       format: 'umd',
     },
     plugins: [
+      resolve(),
       babel({
         externalHelpers: false,
         runtimeHelpers: true,
         exclude: 'node_modules/**',
       }),
-      resolve(),
       commonjs({
         namedExports: {
           'node_modules/humps/humps.js': ['camelizeKeys', 'decamelizeKeys'],
@@ -30,24 +30,5 @@ export default [
       uglify(),
       bundleSize(),
     ],
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/main.js',
-    plugins: [
-      babel({
-        externalHelpers: false,
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-      }),
-      bundleSize(),
-    ],
-    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
   },
 ];
