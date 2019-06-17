3a9808574725214f8d28b78e8221b6cbaae68781
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-17 10:06:26 +0200

    ✨ Only add accessToken header if it is present --- If the asyn method returns undefined, we do not want to add an empty Bearer token to the request header

--- src/utils/createRequestHeaders.js
+++ src/utils/createRequestHeaders.js
@@ -1,5 +1,9 @@
-export default async ({ getAccessToken, version } = {}) => ({
-  'Content-Type': 'application/json',
-  ...(getAccessToken && { Authorization: `Bearer ${await getAccessToken()}` }),
-  ...(version && { 'X-Api-Version': version }),
-});
+export default async ({ getAccessToken, version } = {}) => {
+  let accessToken = undefined;
+  return {
+    'Content-Type': 'application/json',
+    ...(getAccessToken &&
+      (accessToken = await getAccessToken()) !== undefined && { Authorization: `Bearer ${accessToken}` }),
+    ...(version && { 'X-Api-Version': version }),
+  };
+};
