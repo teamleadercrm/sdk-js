b058bf946239d7d164ed1acc72af8cafe7163590
Kemosabert <bert.coppens14@gmail.com>
2019-08-07 11:11:19 +0200

    :fire: Remove the getRequestDomain util function as it's obsolete now

--- test/utils/getRequestDomain.test.js
+++
@@ -1,9 +0,0 @@
-import getRequestDomain from '../../src/utils/getRequestDomain';
-
-describe('get request domain', () => {
-  it('should extract the correct domain from the request url', () => {
-    const requestUrl = 'http://mock.teamleader.api/contacts.list?include=company';
-
-    expect(getRequestDomain(requestUrl)).toBe('contacts');
-  });
-});
