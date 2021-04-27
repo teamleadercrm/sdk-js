7f8787e692d4b621b1ae5ad15816601a316c96e1
Joren Saey <joren.saey@teamleader.eu>
2021-04-27 13:25:53 +0200

    Write and fix tests

---
+++ test/utils/mergeHeaders.test.js
@@ -0,0 +1,60 @@
+import mergeHeaders from '../../src/utils/mergeHeaders';
+
+describe(`merge headers`, () => {
+  it(`should merge the headers in a correct way`, () => {
+    const globalHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+    };
+
+    const localHeaders = {
+      'x-header-3': 'headerValue3',
+      'x-header-4': 'headerValue4',
+    };
+
+    const headers = mergeHeaders(globalHeaders, localHeaders);
+
+    const expectedHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+      'x-header-3': 'headerValue3',
+      'x-header-4': 'headerValue4',
+    };
+
+    expect(headers).toEqual(expectedHeaders);
+  });
+
+  it(`should merge the headers in a correct way when one header object is given`, () => {
+    const globalHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+    };
+
+    const headers = mergeHeaders(globalHeaders);
+
+    const expectedHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+    };
+
+    expect(headers).toEqual(expectedHeaders);
+  });
+
+  it(`should merge the headers in a correct way when one header object is invalid`, () => {
+    const globalHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+    };
+
+    const localHeaders = 'invalid';
+
+    const headers = mergeHeaders(globalHeaders, localHeaders);
+
+    const expectedHeaders = {
+      'x-header-1': 'headerValue1',
+      'x-header-2': 'headerValue2',
+    };
+
+    expect(headers).toEqual(expectedHeaders);
+  });
+});
