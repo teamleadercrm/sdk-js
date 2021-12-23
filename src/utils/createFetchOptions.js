97dce41972cf3923e165fe7aa80621c5242f7821
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-12-23 12:01:01 +0100

    :boom: remove the additionalHeaders property in favour of the fetchOptions object

--- src/utils/createFetchOptions.js
+++ src/utils/createFetchOptions.js
@@ -2,18 +2,14 @@ import createRequestHeaders from './createRequestHeaders';
 import applyPlugins from './applyPlugins';
 
 export default async ({ configuration, parameters } = {}) => {
-  const {
-    getAccessToken,
-    plugins: { request: requestPlugins = [] } = {},
-    version,
-    additionalHeaders = {},
-    fetchOptions,
-  } = configuration;
+  const { getAccessToken, plugins: { request: requestPlugins = [] } = {}, version, fetchOptions = {} } = configuration;
+
+  const { headers = {}, ...rest } = fetchOptions;
 
   return {
-    headers: await createRequestHeaders({ getAccessToken, version, additionalHeaders }),
+    headers: await createRequestHeaders({ getAccessToken, version, additionalHeaders: headers }),
     body: JSON.stringify(applyPlugins(parameters, requestPlugins)),
     method: 'POST',
-    ...fetchOptions,
+    ...rest,
   };
 };
