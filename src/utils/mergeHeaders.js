400412313abd7e70dc8a8e2c2ae869ec663eb3de
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-12-23 12:01:38 +0100

    :fire: remove redundant util

--- src/utils/mergeHeaders.js
+++
@@ -1,7 +0,0 @@
-export default (...headers) =>
-  headers.reduce((combined, headerConfiguration) => {
-    if (!headerConfiguration || typeof headerConfiguration !== 'object') {
-      return combined;
-    }
-    return { ...combined, ...headerConfiguration };
-  }, {});
