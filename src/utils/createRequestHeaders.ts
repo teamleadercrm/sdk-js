fc1811f13c44f0183393d5b4148af94ccfcf3560
Lowie Benoot <lowie@teamleader.eu>
2022-08-19 18:16:33 +0800

    🏷️ converted createRequestHeaders to typescript

--- src/utils/createRequestHeaders.ts
+++ src/utils/createRequestHeaders.ts
@@ -1,4 +1,14 @@
-export default async ({ getAccessToken, version, headers } = {}) => {
+import { Configuration } from '../types';
+
+export default async ({
+  getAccessToken,
+  version,
+  headers,
+}: {
+  getAccessToken?: Configuration['getAccessToken'];
+  version?: Configuration['version'];
+  headers?: HeadersInit;
+} = {}): Promise<HeadersInit> => {
   const accessToken = getAccessToken && (await getAccessToken());
   return {
     'Content-Type': 'application/json',
