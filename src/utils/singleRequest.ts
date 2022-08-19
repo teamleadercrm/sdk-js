dce768a8898a09eb72d7d68a996d59288a8dc0a7
Lowie Benoot <lowie@teamleader.eu>
2022-08-19 18:24:19 +0800

    🏷️ converted singleRequest to typescript

--- src/utils/singleRequest.ts
+++ src/utils/singleRequest.ts
@@ -1,7 +1,12 @@
+import { Configuration } from '../types';
 import checkStatus from './checkStatus';
 import createFetchOptions from './createFetchOptions';
 
-const singleRequest = async ({ url, parameters = {}, configuration = {} } = {}) => {
+const singleRequest = async ({
+  url = '', // @todo probably shouldn't have a default value, but needed to add it beause there is a default {} value
+  parameters = {},
+  configuration = {},
+}: { url?: string; parameters?: Record<string, any>; configuration?: Configuration } = {}) => {
   const fetchOptions = await createFetchOptions({ parameters, configuration });
   const data = await fetch(url, fetchOptions);
   return checkStatus(data);
