5701a23a3191b8eca5f76cef2a1b2e02242cb9a8
Lowie Benoot <lowie@teamleader.eu>
2022-08-19 18:17:03 +0800

    🏷️ converted createRequestUrl to typescript

--- src/utils/createRequestUrl.ts
+++ src/utils/createRequestUrl.ts
@@ -1,2 +1,11 @@
-export default ({ configuration: { baseUrl = '' } = {}, domainName = '', actionName = '' } = {}) =>
-  `${baseUrl}/${domainName}.${actionName}`;
+import { Configuration } from '../types';
+
+export default ({
+  configuration: { baseUrl = '' },
+  domainName,
+  actionName,
+}: {
+  configuration: Configuration;
+  domainName: string;
+  actionName: string;
+}) => `${baseUrl}/${domainName}.${actionName}`;
