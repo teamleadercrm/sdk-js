e75b169e3b3fe9b9247fd5452a53c166ef2b1e37
Lowie Benoot <lowie@teamleader.eu>
2022-08-19 18:26:25 +0800

    🏷️ converted fetchAllRequest to typescript

--- src/utils/fetchAllRequest.ts
+++ src/utils/fetchAllRequest.ts
@@ -1,7 +1,11 @@
 import singleRequest from './singleRequest';
 import merge from 'lodash.merge';
+import { Configuration } from '../types';
 
-const fetchAllRequest = async (config, amountOfRequests) => {
+const fetchAllRequest = async (
+  config: { url: string; parameters: Record<string, any>; configuration: Configuration },
+  amountOfRequests: number,
+) => {
   const data = await Promise.all(
     [...new Array(amountOfRequests - 1)] // -1 because we already did a firstRequest
       .map((_, i) => singleRequest(merge({}, config, { parameters: { page: { number: i + 2 } } }))),
